4edc809ee6cc4c698052525d62f22f6f122c440c
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-08-11 20:40:59 +0000

    Blank letters

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ word_list = ["aardvark", "baboon", "camel"]
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
 chosen_word = random.choice(word_list)
+list = []
 print(chosen_word)
+for blank in range(len(chosen_word)):
+  list.append('_')
+
 
 guess = input("Guess the letter in the word: ").lower()
 
-for char in chosen_word:
-  if char == guess:
-    print('Right')
-  else:
-    print('Wrong')
+for position in range(len(chosen_word)):
+  letter = chosen_word[position]
+  if letter == guess:
+    list[position] = letter
+
+print(list)
